4c0a5a592ac5423a9809636116a27ae8e7244f22
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-02-05 12:31:46 +0100

    show corpus-sum (larger than sum of src=fad words)

--- fad-words.sh
+++ fad-words.sh
@@ -9,6 +9,15 @@ source ~/virt_env/ct-svn/bin/activate
 
 set -e -u
 
+SUM_NOB=$(mktemp -t fad-words.XXXXXXXXXXXX)
+cleanup () {
+    rm -f "${SUM_NOB}"
+    kill $$
+}
+trap cleanup EXIT
+
+
+
 smenob_fad () {
     xmlstarlet sel -t -m '//e[@src="fad"]/mg/tg/t/text()' -c . -n $GTHOME/words/dicts/smenob/src/*.xml
 }
@@ -32,7 +41,7 @@ corp_fad () {
         echo "Assuming corpus already converted …" >&2
     fi
     cat <(ccat -a -l $lang  $GTFREE/converted/$lang/{admin,facta,laws,news,science}) \
-        <(ccat -a -l $lang $GTBOUND/converted/$lang/{admin,facta,laws,news,science})|head -2000
+        <(ccat -a -l $lang $GTBOUND/converted/$lang/{admin,facta,laws,news,science})
 }
 
 preprocess () {
@@ -43,14 +52,18 @@ preprocess () {
 hitparade_fad () {
     unset LC_ALL
     lang=$1
-    corp_fad $lang | preprocess $lang |sort |uniq -c | sed $'s/^ *//;s/ /\t/' | LC_ALL=C sort -k2 -t$'\t'
+    corp_fad $lang | preprocess $lang |\
+      sort | uniq -c | sed $'s/^ *//;s/ /\t/' |\
+      tee >(awk -F'\t' '{sum+=$1} END{print sum}' > "${SUM_NOB}")|\
+      LC_ALL=C sort -k2 -t$'\t'
 }
 
-cleanup () {
-    kill $$
-}
-
-trap cleanup EXIT
 
 LC_ALL=C join -t$'\t' -a1 -11 -22 <(dicts_fad) <(hitparade_fad nob) |\
  awk -F'\t' 'BEGIN{OFS=FS} $2==""{$2=0} {print $2,$1}'
+echo -n $'sum\t'; cat "${SUM_NOB}"
+# This sum is larger than the sum of word-frequencies printed, since
+# we only print those with src=fad-matches
+
+
+# bzcat corpora/*.nb.txt.bz2 ~/corpora/Copyright/bokmål.txt.bz2 | $GTHOME/gt/script/preprocess --abbr=$GTHOME/langs/nob/tools/preprocess/abbr.txt |sort |uniq -c|sed $'s/^ *//;s/ /\t/'|tee >(awk -F'\t' '{sum+=$1} END{print sum}' > SUM_NOB) |LC_ALL=C sort -k2 -t$'\t'  >hitparade_nonfad.tsv   
