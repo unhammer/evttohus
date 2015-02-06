f349c9b2e70cd941a054237c64de8027a1815c82
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-02-06 10:41:14 +0100

    better names; keep plaintext

--- make-freq.sh
+++ make-freq.sh
@@ -8,16 +8,20 @@ trap 'kill 0' EXIT
 lang=$1
 
 if [[ $# -ge 2 ]]; then
-    echo "Converting corpora ..."
+    echo "Running convert2xml ..."
     convert_all $lang
 else
-    echo "Skipping corpus conversion ..."
+    echo "Skipping convert2xml ..."
 fi
 
 test -d freq || mkdir freq
 
-ccat_all $lang | clean_punct | preprocess $lang | xz - > freq/corpus.$lang.xz
+echo "ccat ..."
+ccat_all $lang | xz - >freq/plaincorp.$lang.xz
+echo "preprocess ..."
+xzcat freq/plaincorp.$lang.xz | clean_punct | preprocess $lang | xz - > freq/prepcorp.$lang.xz
 
-xzcat *.$lang.xz | to_freqlist > freq/forms.$lang & pid=$!
-xzcat *.$lang.xz | ana $lang | lemma_per_line | to_freqlist > freq/lms.$lang
+echo "to freqlist ..."
+xzcat freq/prepcorp.$lang.xz | to_freqlist > freq/forms.$lang & pid=$!
+xzcat freq/prepcorp.$lang.xz | ana $lang | lemma_per_line | to_freqlist > freq/lms.$lang
 wait $pid
