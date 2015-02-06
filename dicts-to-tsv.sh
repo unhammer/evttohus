68f2022a65cd5402317c3bcb2ffd5bf3fba00b51
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-02-06 14:44:10 +0100

    convert smesmj/*.csv to tsv, 'normalising' pos names a bit

--- dicts-to-tsv.sh
+++ dicts-to-tsv.sh
@@ -8,6 +8,16 @@ test -d words || mkdir words
 (
     cd words
     dicts2tsv "" nob sma sme smj
+
+    # smesmj has lots of stuff in a csv format that hasn't been converted to XML yet:
+    dir=smesmj
+    for csv in $GTHOME/words/dicts/$dir/src/*.csv; do
+        tsv=$dir/$(basename "$csv")
+        tsv=${tsv%%.csv}.tsv
+        # "normalise" PoS names:
+        tsv=$(echo "$tsv"|perl -wnpe 's/(\w+)_/\u\L$1_/;s/Cc_/CC_/;s/Cs_/CS_/;s/(Pp|Prep)_/Pr_/;s/P_/Po_/;')
+        cut -f1-2 <"${csv}" > "${tsv}"
+    done
 )
 
 test -d words-src-fad || mkdir words-src-fad
