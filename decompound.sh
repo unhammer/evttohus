ea2f1d13830f3f036c118e186525f9b5bea99122
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-02-09 17:03:11 +0100

    argh a hardcoded path

--- decompound.sh
+++ decompound.sh
@@ -39,7 +39,7 @@ test -d out || mkdir out
 test -d out/${dir} || mkdir out/${dir}
 
 for pos in N V A; do
-    dict=words/${dir}/${pos}_smesmj.tsv
+    dict=words/${dir}/${pos}_${dir}.tsv
     echo -n "${pos} compound analyses found: " >&2
     cat <(cut -f1  ${words}/${lang1}???/${pos}_*.tsv) \
         <(cut -f2- ${words}/???${lang1}/${pos}_*.tsv | tr '\t' '\n') \
