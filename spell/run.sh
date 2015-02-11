faf97d64f8456739b6b8d2953d06980676ab684b
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-02-11 14:31:52 +0100

    spell nobsma as well

--- spell/run.sh
+++ spell/run.sh
@@ -2,33 +2,44 @@
 
 set -e -u
 
-dir=smesmj
-
 make
 
 test -d out || mkdir out
-test -d out/${dir} || mkdir out/${dir}
 test -d tmp || mkdir tmp
 
-sed 's/[´`¨~<=>|°·‘§©@€*\&%+́–¼½¾¹]//g' form-freqlist.smj \
-    | cut -f2 \
-    | grep '^[A-Za-zæøåÆØÅöäÖÄáÁŋŊńŃñÑ]\{3,\}$' \
-    | LC_ALL=C sort -u \
-    > tmp/smj.words.sorted
+for lang in sma smj; do
+    sed 's/[´`¨~<=>|°·‘§©@€*\&%+́–¼½¾¹]//g' ../freq/forms.${lang} \
+        | cut -f2 \
+        | grep '^[A-Za-zæøåÆØÅöäÖÄáÁŋŊńŃñÑ]\{3,\}$' \
+        | LC_ALL=C sort -u \
+                > tmp/${lang}.words.sorted
+    ./comp.native tmp/${lang}.words.sorted tmp/${lang}.dawg
+done
 
-./comp.native tmp/smj.words.sorted tmp/smj.dawg
 
-for f in ../out/smesmj/*; do
+spell () {
+    lang=$1
+    f=$2
+    dir=$3
     b=$(basename "$f")
     for edits in 1 2; do
         grep -v '^#' "$f" \
             | cut -f2- \
             | tr '\t' '\n' \
-            | ./spell.native ${edits} tmp/smj.dawg \
+            | ./spell.native ${edits} tmp/${lang}.dawg \
             | tee tmp/${edits}.spelt."$b" \
             | awk -F'\t' '/IN_CORPUS/{next}$2{print}' \
-            > tmp/${edits}.sugg."$b"
+                  > tmp/${edits}.sugg."$b"
         gawk -f join_sugg.awk -vsuggs=tmp/${edits}.sugg."$b" "$f" \
-            > out/${dir}/"$b"_sugg${edits}
+             > out/${dir}/"$b"_sugg${edits}
     done
+}
+
+test -d out/smesmj || mkdir out/smesmj
+for f in ../out/smesmj/*; do
+    spell smj "$f" smesmj
+done
+test -d out/nobsma || mkdir out/nobsma
+for f in ../out/nobsma/*; do
+    spell sma "$f" nobsma
 done
