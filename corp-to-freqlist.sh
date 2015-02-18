f55bc74a5a46e53401ab0f78b05acc3e14a03625
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-02-18 09:28:03 +0100

    doh, forgot to include unpairable lines in combined freq

--- corp-to-freqlist.sh
+++ corp-to-freqlist.sh
@@ -10,7 +10,7 @@ xzcat freq/prepcorp.${lang}.xz | to_freqlist > freq/forms.${lang} & pid=$!
 xzcat freq/prepcorp.${lang}.xz | ana ${lang} | lemma_per_line | to_freqlist > freq/lms.${lang}
 wait ${pid}
 
-LC_ALL=C join -j2 -t$'\t' \
+LC_ALL=C join -e0 -o0,1.1,2.1 -a1 -a2 -j2 -t$'\t' \
     <(LC_ALL=C sort -k2,2 -t$'\t' freq/forms.${lang}) \
     <(LC_ALL=C sort -k2,2 -t$'\t' freq/lms.${lang}) \
     | awk 'BEGIN{OFS=FS="\t"} {print $2+$3,$1}' \
