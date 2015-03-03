8fdfb1f5da6edbadb8581d17d0de8339f19b54ec
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-03-03 14:35:03 +0100

    … actual para hits numbers

--- canonicalise.sh
+++ canonicalise.sh
@@ -15,6 +15,7 @@ else
     echo "Unknown fromlang ${fromlang}" >&2
     exit 1
 fi
+outdir=nob${candlang}sme
 
 pos_glob () {
     case $1 in
@@ -47,8 +48,11 @@ for f in out/${dir}/* spell/out/${dir}/*; do
             >tmp/${dir}/"$b"
     fi
 done
-    
-echo "Add translations from ${fromlang} so we get nob${candlang}sme ..."
+
+echo "Get para hits of all candidates ..."
+para/count-para-hits.sh <(sort -u tmp/${dir}/*) freq/${dir}.lemmas.ids > tmp/${dir}.para-hits
+
+echo "Add translations from ${fromlang} so we get ${outdir} ..."
 for f in tmp/${dir}/*; do
     test -f "$f" || continue
     b=$(basename "$f")
@@ -59,11 +63,11 @@ for f in tmp/${dir}/*; do
         -v fromlang=${fromlang} \
         -v smenob=tmp/smenob -v nobsme=tmp/nobsme \
         -f trans_annotate.awk \
-        >tmp/nob${candlang}sme/"$b"_${fromlang}
+        >tmp/${outdir}/"$b"_${fromlang}
 done
 
 echo "Get main PoS of all candidates ..."
-cut -f2 tmp/nob${candlang}sme/* \
+cut -f2 tmp/${outdir}/* \
     | ana ${candlang} \
     | gawk -F'\t|[+]' '
        $1{ sub(/.*#/,""); pos="nonVNA"; for(i=NF;i>=0;i--)if($i~/^[VNA]$/){pos=$i;break}; ana[$1][pos]++ }
@@ -75,7 +79,7 @@ sumnob=$(awk  -F'\t' '{sum+=$1}END{print sum}' freq/combined.nob)
 sumcand=$(awk -F'\t' '{sum+=$1}END{print sum}' freq/combined.${candlang})
 sumsme=$(awk  -F'\t' '{sum+=$1}END{print sum}' freq/combined.sme)
 echo "Annotate with frequency and whether FST had same-pos analysis ..."
-for f in tmp/nob${candlang}sme/*; do
+for f in tmp/${outdir}/*; do
     b=$(basename "$f")
     pos=$(pos_name "$b")
     echo -n "$b "
@@ -92,11 +96,11 @@ for f in tmp/nob${candlang}sme/*; do
             # Let field 7 be true iff the FST gave a same-pos analysis:
             BEGIN{ OFS=FS="\t"; while(getline<posf)ana[$2][$1]++ }
             { print $1,$2,$3,$4,$5,$6,$2 in ana[pos] }' \
-        | gawk -v from=${fromfield} -v hitsf=<(para/count-para-hits.sh "$f" freq/${dir}.lemmas.ids) '
+        | gawk -v from=${fromfield} -v hitsf=tmp/${dir}.para-hits '
             # Let field 8 be count of hits in parallel sentences:
-            BEGIN{ OFS=FS="\t"; while(getline<hitsf) hits[$1][$2]++ }
+            BEGIN{ OFS=FS="\t"; while(getline<hitsf) hits[$2][$3]=$1 }
             !($from in hits) || !($2 in hits[$from]) { hits[$from][$2]=0 }
             { print $1,$2,$3,$4,$5,$6,$7,hits[$1][$2] } ' \
-        >out/nob${candlang}sme/"$b"
+        >out/${outdir}/"$b"
 done
 echo
