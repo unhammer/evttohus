591d2ba155d09df677059aadd2fb5a0652a660ac
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-06-03 12:39:51 +0200

    handle para-hits for /-joined translations

--- para/count-para-hits.sh
+++ para/count-para-hits.sh
@@ -8,10 +8,25 @@ candidates=$1
 lemmapair_ids=$2
 # ^ from join-lemmas-on-ids.sh
 fromfield=$3
+# fromfield might contain several /-separated translations
 
 <"${lemmapair_ids}" gawk \
     -v fromfield=${fromfield} \
-    -v f=$candidates '
-  BEGIN{ OFS=FS="\t"; while(getline<f) cand[$fromfield][$2]++ }
-  $2 in cand && $3 in cand[$2] { print $2,$3 }
+    -v candf=$candidates '
+  BEGIN { 
+    OFS=FS="\t"
+    while(getline<candf) {
+      n=split($fromfield, froms, "/")
+      for(i=1;i<=n;i++) {
+        from=froms[i]
+        cand[from][$2][$fromfield]++
+      }
+    }
+  }
+  $2 in cand && $3 in cand[$2] { 
+    for(joined in cand[$2][$3]) {
+      print joined, $3
+      # "joined" might be a single translation, or several /-joined translations
+    }
+  }
   ' | to_freqlist
