d66e4581bf59a88b992ee248c8517645ec9295a1
Kevin Brubeck Unhammer <unhammer+apertium@mm.st>
2015-06-29 13:54:37 +0200

    indianer→indiánar

--- nob2smj-loan.sh
+++ nob2smj-loan.sh
@@ -5,8 +5,8 @@ set -e -u
 source functions.sh
 
 # divvun.no runs an old version of bash, so we use a plain array where keys have to match up:
-src=(     itet      i     og      é      e      et      ek     ol     em     om    ør  ie )
-trg=( itiehtta  iddja  åvggå  iedja  iedja  iehtta  iehkka  åvllå  iebma  åvmmå  erra  ij )
-pos=(        N      N      N      N      N       N       N      N      N      N     N   N )
+src=(     itet      i     og      é      e      et      ek     ol     em     om    ør  ie  er aner )
+trg=( itiehtta  iddja  åvggå  iedja  iedja  iehtta  iehkka  åvllå  iebma  åvmmå  erra  ij  ar ánar )
+pos=(        N      N      N      N      N       N       N      N      N      N     N   N   N    N )
 
 loans nob smj "$1"
